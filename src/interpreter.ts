b14fb3637142683df91edc460ff133a6c3007b00
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2022-06-18 10:22:09 +0900

    Update interpreter to read line

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -1,1 +1,19 @@
-console.log('hello')
+import readline from 'readline'
+
+function showPrompt() {
+  process.stdout.write('> ')
+}
+
+function main() {
+  const reader = readline.createInterface({
+    input: process.stdin
+  })
+
+  showPrompt()
+  reader.on('line', (line) => {
+    console.log(line)
+    showPrompt()
+  })
+}
+
+main()
